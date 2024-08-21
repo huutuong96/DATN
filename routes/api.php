f58bcf68d137eaf5ec4de576ddd311e29573079b
unknown <nguyenhuutuong.na@gmail.com>
2024-08-21 15:13:42 +0700

    test

--- routes/api.php
+++ routes/api.php
@@ -10,6 +10,7 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\BannerController;
 use App\Http\Controllers\FAQController;
 use App\Http\Controllers\TaxController;
+use App\Http\Controllers\PermissionsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,29 +23,32 @@ use App\Http\Controllers\TaxController;
 |
 */
 
-Route::group(['middleware' => 'checkToken'], function () {
+Route::group(['middleware' => ['checkToken', 'CheckStatusUser', 'CheckRole', 'CheckPermission']], function () {
+
+
     Route::resource('user', AuthenController::class);
     Route::post('user/me', [AuthenController::class, "me"]);
 
     Route::resource('role', RolesController::class);
-    
+    Route::resource('rank', RanksController::class);
     Route::resource('address', AddressController::class);
+    Route::resource('permission', PermissionsController::class);
+
+    Route::post('permission/grant_access', [PermissionsController::class, "grant_access"]);
+    Route::post('permission/delete_access', [PermissionsController::class, "delete_access"]);
+
+    //tường làm
+    Route::resource('banners', BannerController::class);
+    Route::resource('faqs', FAQController::class);
+    Route::resource('taxs', TaxController::class);
+    Route::resource('ranks', RanksController::class);
 
 });
 
-//tường làm
-Route::resource('banners', BannerController::class);
-Route::resource('faqs', FAQController::class);
-Route::resource('taxs', TaxController::class);
-Route::resource('ranks', RanksController::class);
+
 
 
 
 Route::post('user/register', [AuthenController::class, "register"]);
 Route::post('user/login', [AuthenController::class, "login"]);
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-
-Route::resource('users', UserController::class);
