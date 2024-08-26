f28dc8f47687254667fde82c2ef57a6eec1059ef
ngothi <ngodinhtungthi@gmail.com>
2024-08-26 08:12:01 +0700

    programe, voucher ...

---
+++ app/Models/Voucher.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Voucher extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'type',
+        'status',
+        'create_by',
+        'update_by',
+        'created_at',
+        'updated_at',
+        'user_id',
+        'code',
+    ];
+}
