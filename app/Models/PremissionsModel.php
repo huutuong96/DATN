e89d263b2e57045294379214805d8d7660a47d93
Nguyễn Hữu Tường <nguyenhuutuong.na@gmail.com>
2024-08-29 11:04:07 +0700

    fix name premission and role premission

--- app/Models/PremissionsModel.php
+++ app/Models/PremissionsModel.php
@@ -5,18 +5,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class role_permissionModel extends Model
+class premissionsModel extends Model
 {
     use HasFactory;
-    protected $table = 'role_premissions'; // Thay đổi tên bảng nếu cần
+    protected $table = 'premissions'; // Thay đổi tên bảng nếu cần
 
     // Các trường có thể được gán hàng loạt
     protected $fillable = [
-        'role_id',
-        'premission_id',
+        'premissionName',
         'status',
-        'created_at',
+        'create_by',
         'update_by',
     ];
 }
-
