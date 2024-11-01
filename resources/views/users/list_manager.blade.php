97dc997b13e620b5f57920808cea6e07524e512d
Nguyễn Hữu Tường <nguyenhuutuong.na@gmail.com>
2024-11-01 09:50:11 +0700

    update code 11/1

--- resources/views/users/list_manager.blade.php
+++ resources/views/users/list_manager.blade.php
@@ -35,6 +35,128 @@
                         >
                             User đã xóa
                         </a>
+                        <div style="width: 20px;"></div> 
+                        <!-- Toggle Between Modals -->
+                    <button type="button" class="btn btn-primary " data-bs-toggle="modal" data-bs-target="#firstmodal">Thêm tài khoản</button>
+                    <!-- First modal dialog -->
+                    <div class="modal fade" id="firstmodal" aria-hidden="true" aria-labelledby="..." tabindex="-1">
+                        <div class="modal-dialog modal-dialog-centered">
+                            <div class="modal-content">
+                                <div class="modal-body text-center p-5">
+                                <form id="addUser">
+                                    <div class="row">
+                                        <div class="col-6">
+                                            <div class="mb-3">
+                                                <label for="firstNameinput" class="form-label">First Name</label>
+                                                <input type="text" class="form-control" placeholder="Enter your firstname" id="firstNameinput">
+                                            </div>
+                                        </div><!--end col-->
+                                        <div class="col-6">
+                                            <div class="mb-3">
+                                                <label for="lastNameinput" class="form-label">Last Name</label>
+                                                <input type="text" class="form-control" placeholder="Enter your lastname" id="lastNameinput">
+                                            </div>
+                                        </div><!--end col-->
+                                        <div class="col-12">
+                                            <div class="mb-3">
+                                                <label for="compnayNameinput" class="form-label">Company Name</label>
+                                                <input type="text" class="form-control" placeholder="Enter company name" id="compnayNameinput">
+                                            </div>
+                                        </div><!--end col-->
+                                        <div class="col-6">
+                                            <div class="mb-3">
+                                                <label for="phonenumberInput" class="form-label">Phone Number</label>
+                                                <input type="tel" class="form-control" placeholder="+(245) 451 45123" id="phonenumberInput">
+                                            </div>
+                                        </div><!--end col-->
+                                        <div class="col-6">
+                                            <div class="mb-3">
+                                                <label for="emailidInput" class="form-label">Email Address</label>
+                                                <input type="email" class="form-control" placeholder="example@gamil.com" id="emailidInput">
+                                            </div>
+                                        </div><!--end col-->
+                                        <div class="col-12">
+                                            <div class="mb-3">
+                                                <label for="address1ControlTextarea" class="form-label">Address</label>
+                                                <input type="text" class="form-control" placeholder="Address 1" id="address1ControlTextarea">
+                                            </div>
+                                        </div><!--end col-->
+                                        <div class="col-6">
+                                            <div class="mb-3">
+                                                <label for="citynameInput" class="form-label">City</label>
+                                                <input type="email" class="form-control" placeholder="Enter your city" id="citynameInput">
+                                            </div>
+                                        </div><!--end col-->
+                                        <div class="col-6">
+                                            <div class="mb-3">
+                                                <label for="ForminputState" class="form-label">State</label>
+                                                <select id="ForminputState" class="form-select">
+                                                    <option selected>Choose...</option>
+                                                    <option>...</option>
+                                                </select>
+                                            </div>
+                                        </div><!--end col-->
+                                        <div class="col-lg-12">
+                                            <div class="text-end">
+                                                <button type="submit" class="btn btn-primary">Submit</button>
+                                            </div>
+                                        </div><!--end col-->
+                                    </div><!--end row-->
+                                </form>
+
+                                <script>
+                                document.getElementById('addCategoryForm').addEventListener('submit', async function (event) {
+                                    event.preventDefault(); // Ngăn chặn hành vi mặc định của form
+                                    await addCategory();
+                                });
+
+                                async function addCategory() {
+                                    const urlParams = new URLSearchParams(window.location.search);
+                                    const token = urlParams.get('token');
+                                    console.log(token);
+                                    
+                                    const title = document.getElementById('title').value;
+                                    const index = document.getElementById('index').value;
+                                    const image = document.getElementById('image').files; // Nếu bạn có URL của ảnh
+                                    const status = document.getElementById('status').value;
+                                    const parentId = document.getElementById('parentId').value;
+                                    console.log(image);
+                                    
+                                    const data = {
+                                        title: title,
+                                        index: index,
+                                        image: image,
+                                        status: status,
+                                        parent_id: parentId,
+                                        update_by: {{ auth()->user()->id }}
+                                    };
+
+                                    try {
+                                        const res = await fetch(`https://vnshop.top/api/categories`, {
+                                            method: 'POST',
+                                            headers: {
+                                                'Content-Type': 'application/json',
+                                                'Authorization': `Bearer ${token}` // Gắn Bearer Token vào header
+                                            },
+                                            body: JSON.stringify(data)});
+                                        const payload = await res.json();
+                                        if(!res.ok) {
+                                            throw new Error('Network response was not ok');
+                                        }
+                                        alert('Thêm danh mục thành công!');
+                                        window.location.reload();
+                                        } catch (error) {
+                                        console.error('Error:', error);
+                                        alert('Đã xảy ra lỗi: ' + error.message);
+                                        }
+                                }
+                                </script>
+
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                     </div><!-- end card header -->
     
                     <div class="card-body">
