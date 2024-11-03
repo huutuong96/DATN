febc9a7e00f6656a1aad53cd61ee236c9eebe104
ngothi <ngodinhtungthi@gmail.com>
2024-11-03 18:32:01 +0700

    a

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -6,7 +6,7 @@ use App\Models\Cart_to_usersModel;
 use App\Models\ProducttocartModel;
 use App\Models\product_variants;
 use App\Models\Product;
-use App\Models\shop;
+use App\Models\Shop;
 use App\Models\variantattribute;
 use Illuminate\Http\Request;
 use Tymon\JWTAuth\Facades\JWTAuth;
