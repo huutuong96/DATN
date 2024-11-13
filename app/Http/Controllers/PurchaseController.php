3aebc832921bb782fa8e074d7aff4028813988c7
ngothi <ngodinhtungthi@gmail.com>
2024-11-13 13:53:22 +0700

    a

--- app/Http/Controllers/PurchaseController.php
+++ app/Http/Controllers/PurchaseController.php
@@ -80,7 +80,7 @@ class PurchaseController extends Controller
         try {
             DB::beginTransaction();
             $payment = PaymentsModel::where('id', $request->payment)->first();
-            dd($payment);
+            dd($request->payment);
             if (!$payment) {
                 return response()->json([
                     'status' => false,
