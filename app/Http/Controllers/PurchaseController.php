4b3fb8051f6ad3490c5e5b76a66436682e500fb5
ngothi <ngodinhtungthi@gmail.com>
2024-11-19 21:38:08 +0700

    sscsac

--- app/Http/Controllers/PurchaseController.php
+++ app/Http/Controllers/PurchaseController.php
@@ -206,7 +206,7 @@ class PurchaseController extends Controller
                     'email' => auth()->user()->email,
                 ]);
 
-
+                dd($total_amount);
                 $url = $PaymentsController->vnpay_payment($request, $total_amount, $groupOrderIds);
                 return response()->json([
                     'status' => true,
