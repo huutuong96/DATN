13d33ab8643fe7aa4450359ef1906c95a9aae22d
ngothi <ngodinhtungthi@gmail.com>
2024-11-20 12:38:36 +0700

    sssccc

--- app/Http/Controllers/PurchaseController.php
+++ app/Http/Controllers/PurchaseController.php
@@ -211,7 +211,7 @@ class PurchaseController extends Controller
             }
             $user = jwtAuth::parseToken()->authenticate();
             SendMail::dispatch($orders, $total_amount, $carts, $orderDetails, $shipFee, $products, $variants, auth()->user()->email, $payment->name, $user, $discountMainVoucher);
-            SendNotification::dispatch('Đặt hàng thành công', "Mã đơn hàng: $groupOrderIds", auth()->id(), $groupOrderIds);
+            SendNotification::dispatch('Đặt hàng thành công', "Mã đơn hàng: $groupOrderIds", auth()->id(), $groupOrderIds, null);
             ProducttocartModel::whereIn('id', $request->carts)->delete();
             if ($payment->code == 'VNPAY') {
                 $PaymentsController = new PaymentsController();
