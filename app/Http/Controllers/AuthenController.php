40f3a942c6e275e234b7b071d8a0788716b60c86
ngothi <ngodinhtungthi@gmail.com>
2024-11-22 14:19:48 +0700

    sadasdsadasdasda

--- app/Http/Controllers/AuthenController.php
+++ app/Http/Controllers/AuthenController.php
@@ -210,7 +210,7 @@ class AuthenController extends Controller
             "password" => Hash::make($request->password),
             "email" => $request->email,
             "rank_id" => $request->rank_id ?? null,
-            "role_id" => $request->role_id ?? null,
+            "role_id" => 1,
             "status" => 101, // 101 là tài khoản chưa được kích hoạt
             "login_at" => now(),
         ];
