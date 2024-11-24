d9bc164b2a375b992651575d26d8f998e7da7608
ngothi <ngodinhtungthi@gmail.com>
2024-11-24 15:41:22 +0700

    sadsadsadsadsa

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -45,7 +45,7 @@ class OrdersController extends Controller
         
         $user = JWTAuth::parseToken()->authenticate();
         $order_status = $request->order_status ?? 1;
-        $orders = OrdersModel::with(['orderDetails.variant.product', 'shop']) // Eager load 'product' qua 'orderDetails'
+        $orders = OrdersModel::with(['orderDetails.variant.product', 'shop','payment']) // Eager load 'product' qua 'orderDetails'
             ->where('user_id', $user->id)
             ->where('order_status', $order_status)
             ->orderby('created_at', 'desc')
