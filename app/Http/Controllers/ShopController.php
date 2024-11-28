147cce0b5b13b6b1aab332461de08a68d42e5035
VietHoang04 <tranluuviethoang.com>
2024-11-28 19:09:00 +0700

    fixx ok

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -1141,7 +1141,6 @@ class ShopController extends Controller
 
     public function getShopByCategory(Request $request)
     {
-        dd($request);
         $limit = $request->limit ?? 20;
         $query = CategoriesModel::query();
         if ($request->has('category_id')) {
