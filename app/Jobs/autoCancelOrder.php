05d70cb213af8dd451c403fcdfad982f089a5fbf
ngothi <ngodinhtungthi@gmail.com>
2024-12-01 16:03:44 +0700

    sadsada

--- app/Jobs/autoCancelOrder.php
+++ app/Jobs/autoCancelOrder.php
@@ -24,7 +24,7 @@ class autoCancelOrder implements ShouldQueue
     {
             $order = OrdersModel::find($this->order->id);
             if ($order) {
-                $order->update(['order_status' => 3]);
+                $order->update(['order_status' => 10]);
             }
     }
 }
