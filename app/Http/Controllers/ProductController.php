41ac633008ea4fcd883eb3e4994386f345d365b5
ngothi <ngodinhtungthi@gmail.com>
2024-12-03 13:36:37 +0700

    ádsadsadsa

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -9,6 +9,7 @@ use App\Exports\OrderExport;
 use App\Exports\ProductsExport;
 use App\Exports\SellerExport;
 use App\Exports\ShopExport;
+use App\Exports\Transaction_history;
 use App\Exports\UserExport;
 use App\Models\CategoriesModel;
 use Illuminate\Http\Request;
@@ -1453,6 +1454,9 @@ public function ProductAll(Request $request)
             }
             if ($request->data == 'transaction_history') {
                 return Excel::download(new Transaction_history($request), 'vnshop-transaction_history.xlsx');
+            }
+            if ($request->data == 'order_details') {
+                return Excel::download(new Transaction_history($request), 'vnshop-order_details.xlsx');
             }
         } catch (\Throwable $th) {
             return 'export thất bại: ' . $th->getMessage();
