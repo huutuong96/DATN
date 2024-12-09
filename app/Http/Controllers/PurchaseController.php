1f49ee4fbec7655a6fd9342452c34f2ccd68d94f
Nguyễn Hữu Tường <nguyenhuutuong.na@gmail.com>
2024-12-09 13:43:08 +0700

    fix send mail order to cart

--- app/Http/Controllers/PurchaseController.php
+++ app/Http/Controllers/PurchaseController.php
@@ -255,7 +255,7 @@ class PurchaseController extends Controller
                         'ship_fee' => $shipFee,
                         'email' => auth()->user()->email,
                     ]);
-                        // ProducttocartModel::whereIn('id', $request->carts)->delete();
+                        ProducttocartModel::whereIn('id', $request->carts)->delete();
                     $url = $PaymentsController->vnpay_payment($request, $total_amount, $groupOrderIds);
                     return response()->json([
                         'status' => true,
@@ -263,8 +263,8 @@ class PurchaseController extends Controller
                         'url' => $url,
                     ], 200);
                 }  
-                // SendMail::dispatch($orders, $total_amount, $carts, $orderDetails, $shipFee, $products, $variants, auth()->user()->email, $payment->name, $user, $discountMainVoucher);
-                return view('test', compact('orders', "total_amount", "carts", "orderDetails", "shipFee", "products", "variants"));
+                SendMail::dispatch($orders, $total_amount, $carts, $orderDetails, $shipFee, $products, $variants, auth()->user()->email, $payment->name, $user, $discountMainVoucher);
+                // return view('test', compact('orders', "total_amount", "carts", "orderDetails", "shipFee", "products", "variants"));
                 SendNotification::dispatch('Đặt hàng thành công', "Mã đơn hàng: $groupOrderIds", auth()->id(), $groupOrderIds, null);
                 ProducttocartModel::whereIn('id', $request->carts)->delete();
                 // deleteProductToCart::dispatch($request->carts);   
