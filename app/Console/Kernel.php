4f9e6887893406abaac348dbbd80bd15bf511f98
ngothi <ngodinhtungthi@gmail.com>
2024-12-10 08:06:13 +0700

    gggggggggggggggggggggg

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,26 +12,26 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('queue:work --stop-when-empty')
-        // ->everyMinute()
-        // ->withoutOverlapping();
+        $schedule->command('queue:work redis --stop-when-empty')
+        ->everyMinute()
+        ->withoutOverlapping();
 
-        // // Add this method to your Kernel.php file to schedule the cancel_order_auto method to run every minute
-        // $schedule->call(function () {
-        //     app(\App\Http\Controllers\OrdersController::class)->cancel_order_auto();
-        // })->daily();
+        // Add this method to your Kernel.php file to schedule the cancel_order_auto method to run every minute
+        $schedule->call(function () {
+            app(\App\Http\Controllers\OrdersController::class)->cancel_order_auto();
+        })->daily();
 
-        // $schedule->call(function () {
-        //     app(\App\Http\Controllers\NotificationController::class)->send_mail_event();
-        // })->daily();
+        $schedule->call(function () {
+            app(\App\Http\Controllers\NotificationController::class)->send_mail_event();
+        })->daily();
 
-        // $schedule->call(function () {
-        //     app(\App\Http\Controllers\NotificationController::class)->check_time_event();
-        // })->daily();
+        $schedule->call(function () {
+            app(\App\Http\Controllers\NotificationController::class)->check_time_event();
+        })->daily();
 
-        // $schedule->call(function () {
-        //     app(\App\Http\Controllers\VnshopController::class)->checkWebDie();
-        // })->everyTenMinutes();
+        $schedule->call(function () {
+            app(\App\Http\Controllers\VnshopController::class)->checkWebDie();
+        })->everyTenMinutes();
     }
 
     /**
