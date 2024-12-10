8cfdf437fc4459f62dca48970a1fa211c7cc4c18
ngothi <ngodinhtungthi@gmail.com>
2024-12-10 08:16:58 +0700

    fffffffffffffffffffffffffff

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('queue:work redis --stop-when-empty')
+        $schedule->command('queue:work --stop-when-empty')
         ->everyMinute()
         ->withoutOverlapping();
 
