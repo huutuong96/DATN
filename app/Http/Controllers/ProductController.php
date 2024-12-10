fc0d433a04e9df0e58fd6d3d6daf6840d146c65c
VietHoang04 <tranluuviethoang.com>
2024-12-10 15:11:30 +0700

    update

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -789,89 +789,80 @@ class ProductController extends Controller
         $product->save();
         return redirect()->back()->with('success', 'Duyệt sản phẩm thành công');
     }
-    // public function updateProduct(Request $request, string $id)
-    // // ProductRequest
-    // {
+    public function updateProduct(Request $request, string $id)
+    // ProductRequest
+    {
         
-    //     $product = Product::find($id); 
-    //     if (!$product) {
-    //         return response()->json([
-    //             'status' => false,
-    //             'message' => "Sản phẩm không tồn tại",
-    //         ], 404);
-    //     }
+        $product = Product::find($id); 
+        if (!$product) {
+            return response()->json([
+                'status' => false,
+                'message' => "Sản phẩm không tồn tại",
+            ], 404);
+        }
+        Image::where('product_id',$product->id)->delete();
+        foreach ($request->images as $image) {
+            $imageModel = Image::create([
+                'product_id' => $product->id,
+                'url' => $image ?? null,
+                'status' => 1,
+            ]);
+        }
+        $user = JWTAuth::parseToken()->authenticate();
 
-    //     $user = JWTAuth::parseToken()->authenticate();
-
-    //     if($request->name != $product->name){
-    //         $slug = Str::slug($request->name);
-    //     }else{
-    //         $slug = $product->slug;
-    //     }
-
-    //         if($request->name != $product->name){
-    //             $slug = Str::slug($request->name);
-    //         }else{
-    //             $slug = $product->slug;
-    //         }
-    //     $dataInsert = [
-    //         'product_id' => $product->id,
-    //         'name' => $request->name ?? $product->name,
-    //         'sku' => $request->sku ?? null,
-    //         'slug' => $slug,
-    //         'description' => $request->description ?? $product->description,
-    //         'infomation' => $request->infomation ?? $product->infomation ,
-    //         'price' => $request->variantMode ? 0 : $request->price, // nếu có biến thể thì nó = 0
-    //         'sale_price' => $request->sale_price ?? $product->sale_price,
-    //         'image' => $product->image,
-    //         'quantity' => $request->stock ?? $product->quantity,
-    //         'parent_id' => $request->parent_id ?? $product->parent_id,
-    //         'update_by' => $user->id,
-    //         'category_id' => $request->category ?? $product->category_id,
-    //         'shop_id' => $request->shop_id ?? $product->shop_id,
-    //         'height' => $request->height ?? $product->height,
-    //         'length' => $request->length ?? $product->length,
-    //         'weight' => $request->weight ?? $product->weight,
-    //         'width' => $request->width ?? $product->width,
-    //         'status' =>  $product->status,
-    //         'created_at' => $product->created_at,
-    //         'show_price' => $request->show_price ?? $product->show_price,
-    //         'brand' => $request->brand ?? $product->brand,
-    //         'json_variants' => $product->json_variants ,
-    //         'admin_note' => $request->admin_note ?? $product->admin_note,
-    //         'is_delete' => $request->is_delete ?? $product->is_delete,
-    //         'updated_at' => now(),
+        if($request->name != $product->name){
+            $slug = Str::slug($request->name);
+        }else{
+            $slug = $product->slug;
+        }
+        
+        $dataInsert = [
+            'product_id' => $product->id,
+            'name' => $request->name ?? $product->name,
+            'sku' => $request->sku ?? null,
+            'slug' => $slug,
+            'description' => $request->description ?? $product->description,
+            'infomation' => $request->infomation ?? $product->infomation ,
+            'price' => $request->variantMode ? 0 : $request->price, // nếu có biến thể thì nó = 0
+            'sale_price' => $request->sale_price ?? $product->sale_price,
+            'image' => $request->images[0],
+            'quantity' => $request->stock ?? $product->quantity,
+            'parent_id' => $request->parent_id ?? $product->parent_id,
+            'update_by' => $user->id,
+            'category_id' => $request->category ?? $product->category_id,
+            'shop_id' => $request->shop_id ?? $product->shop_id,
+            'height' => $request->height ?? $product->height,
+            'length' => $request->length ?? $product->length,
+            'weight' => $request->weight ?? $product->weight,
+            'width' => $request->width ?? $product->width,
+            'status' =>  $product->status,
+            'created_at' => $product->created_at,
+            'show_price' => $request->show_price ?? $product->show_price,
+            'brand' => $request->brand ?? $product->brand,
+            'json_variants' => $product->json_variants ,
+            'admin_note' => $request->admin_note ?? $product->admin_note,
+            'is_delete' => $request->is_delete ?? $product->is_delete,
+            'updated_at' => now(),
             
-    //         'update_version' => $product->update_version + 1,
-    //         'change_of' => $request->variantMode === true ? json_encode($request->variant) : json_encode(0) ,
-    //     ];
+            'update_version' => $product->update_version + 1,
+            'change_of' => $request->variantMode === true ? json_encode($request->variant) : json_encode(0) ,
+        ];
         
-    //     try {
-    //         DB::table('update_product')->insert($dataInsert);
-    //         if ($request->hasFile('images')) {
-    //             foreach ($request->file('images') as $image) {
-    //                 $uploadedImage = $cloudinary->uploadApi()->upload($image->getRealPath());
-    //                 $imageUrl = $uploadedImage['secure_url'];
-
-    //                 Image::create([
-    //                     'product_id' => $product->id,
-    //                     'url' => $imageUrl,
-    //                     'status' => 0,
-    //                 ]);
-    //             }
-    //         }
-    //         return response()->json([
-    //             'status' => true,
-    //             'message' => "Yêu cầu của bạn đã được gửi vui lòng chờ xét duyệt"
-    //         ], 200);
-    //     } catch (\Throwable $th) {
-    //         return response()->json([
-    //             'status' => false,
-    //             'message' => "Yêu cầu Cập nhật không thành công",
-    //             'error' => $th->getMessage(),
-    //         ], 500);
-    //     }
-    // }
+        try {
+            DB::table('update_product')->insert($dataInsert);
+            
+            return response()->json([
+                'status' => true,
+                'message' => "Yêu cầu của bạn đã được gửi vui lòng chờ xét duyệt"
+            ], 200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'message' => "Yêu cầu Cập nhật không thành công",
+                'error' => $th->getMessage(),
+            ], 500);
+        }
+    }
 
     
     // public function handleUpdateProduct(Request $request, string $id)
