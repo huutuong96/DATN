bf28fa8b1099fea667e3b7ad76620252879dca6b
VietHoang04 <tranluuviethoang.com>
2024-12-12 11:53:36 +0700

    fixx

--- app/Http/Requests/VoucherRequest.php
+++ app/Http/Requests/VoucherRequest.php
@@ -27,13 +27,12 @@ class VoucherRequest extends FormRequest
         'title' => 'required|string|max:255',
         'description' => 'nullable|string',
         'quantity' => 'required|integer|min:1',
+        'limitValue'=>'required|integer|min:1',
+        'min'=>'required|integer|min:1',
         'condition' => 'nullable',
         'ratio' => 'nullable|numeric|min:0', 
         'code' => 'nullable|string|max:100',
         'status' => 'required',
-        'amount' => 'required|integer|min:1',
-        'count' => 'required|integer|min:1',
-       
     ];
 }
 
@@ -42,24 +41,26 @@ class VoucherRequest extends FormRequest
      *
      * @return array<string, string>
      */
-    public function messages(): array
-    {
-        return [
-            'title.required' => 'Trường tiêu đề là bắt buộc.',
-            'title.string' => 'Trường tiêu đề phải là một chuỗi ký tự.',
-            'title.max' => 'Trường tiêu đề không được vượt quá 255 ký tự.',
-            'description.string' => 'Trường mô tả phải là một chuỗi ký tự.',
-            'quantity.required' => 'Trường số lượng là bắt buộc.',
-            'quantity.integer' => 'Trường số lượng phải là một số nguyên.',
-            'quantity.min' => 'Trường số lượng phải lớn hơn hoặc bằng 0.',
-            'condition.string' => 'Trường điều kiện phải là một chuỗi ký tự.',
-            'ratio.numeric' => 'Trường tỷ lệ phải là một số.',
-            'code.string' => 'Trường mã phải là một chuỗi ký tự.',
-            'code.max' => 'Trường mã không được vượt quá 100 ký tự.',
-            'status.required' => 'Trường trạng thái là bắt buộc.',
-            'status.string' => 'Trường trạng thái phải là một chuỗi ký tự.',
-        ];
-    }
+   public function messages(): array
+{
+    return [
+        'title.required' => 'Trường tiêu đề là bắt buộc.',
+        'title.string' => 'Trường tiêu đề phải là một chuỗi ký tự.',
+        'title.max' => 'Trường tiêu đề không được vượt quá 255 ký tự.',
+        'description.string' => 'Trường mô tả phải là một chuỗi ký tự.',
+        'quantity.required' => 'Trường số lượng là bắt buộc.',
+        'quantity.integer' => 'Trường số lượng phải là một số nguyên.',
+        'quantity.min' => 'Trường số lượng phải lớn hơn hoặc bằng 0.',
+        'condition.string' => 'Trường điều kiện phải là một chuỗi ký tự.',
+        'ratio.numeric' => 'Trường tỷ lệ phải là một số.',
+        'limitValue.integer'=>'Trường số nguyên',
+        'min'=>'Nhập số tiền đơn hàng tối thiểu được áp dụng',
+        'code.string' => 'Trường mã phải là một chuỗi ký tự.',
+        'code.max' => 'Trường mã không được vượt quá 100 ký tự.',
+        'status.required' => 'Trường trạng thái là bắt buộc.',
+        'status.string' => 'Trường trạng thái phải là một chuỗi ký tự.',
+    ];
+}
 
     /**
      * Handle a failed validation attempt.
